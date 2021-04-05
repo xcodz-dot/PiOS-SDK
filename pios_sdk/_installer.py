3be7cc46541af1559cbc4828e18ff5c3c6069e56
xcodz-dot <xcodz-dot@users.noreply.github.com>
2021-04-05 20:31:57 +0530

    SDK Updates to install in partial_uninstall environments

--- pios_sdk/_installer.py
+++ pios_sdk/_installer.py
@@ -16,6 +16,6 @@ def install_app(root: str, assets_root: str, file_root: str):
     shutil.copyfile(f"{file_root}/main.py", f"{root}/main.py")
     shutil.copytree(f"{file_root}/packages", f"{root}/packages")
     shutil.copyfile(f"{file_root}/app.json", f"{root}/app.json")
-    shutil.copytree(f"{file_root}/assets", assets_root)
+    shutil.copytree(f"{file_root}/assets", assets_root, dirs_exist_ok=True)
 
     return True
